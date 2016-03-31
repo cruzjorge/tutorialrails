4016a45f3db7a7663481a50657434170697dda36
Jorge Cruz <cruzjorge.mx@gmail.com>
2016-03-30 23:32:01 -0600

    Siguiendo curso hasta video 10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,20 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    #GET /articles/new
+    def new
+        @article = Article.new
+    end
+
+    #POST /articles
+    def create
+        @article = Article.new(title: params[:article][:title],
+                                body: params[:article][:body])
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
 end
