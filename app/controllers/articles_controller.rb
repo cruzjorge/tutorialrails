b8b9039fc2784d6fd686c3e83f966d659e62559f
Jorge Cruz <cruzjorge.mx@gmail.com>
2016-04-03 20:52:18 -0500

    Siguiendo curso hasta video 13

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     #POST /articles
     def create
         @article = Article.new(article_params)
@@ -26,13 +30,22 @@ class ArticlesController < ApplicationController
 
     #DELETE /articles/:id
     def destroy
+        #DELETE FROM articles
         @article = Article.find(params[:id])
-        @article.destroy
+        @article.destroy # Destroy eliminar el objeto de la BD
         redirect_to articles_path
     end
 
     #PUT /articles/:id
     def update
+        #UPDATE
+        # @article.update_attributes({title: 'Nuevo titulo'})
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render :edit
+        end
     end
 
     private
